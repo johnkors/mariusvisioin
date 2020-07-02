12d419ec1c050a10ef9a0510b257521783e3f2b6
John Korsnes <johnkors@gmail.com>
2020-07-02 13:00:51 +0200

    Schmol sample

---
+++ injoice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Azure;
+using Azure.AI.FormRecognizer;
+using Azure.AI.FormRecognizer.Models;
+using Azure.AI.FormRecognizer.Training;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace injoice
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = Authenticate("https://john-cs-compvision-test.cognitiveservices.azure.com/", "");
+        }
+
+        public static FormRecognizerClient Authenticate(string endpoint, string key)
+        {
+            var credential = new AzureKeyCredential(key);
+            return new FormRecognizerClient(new Uri(endpoint), credential);
+        }
+    }
+}
